4612f2f5aa2510e9b6c05ad18e03ec703a565f0c
gsolazaro <gsolazaro@gmail.com>
2023-01-27 13:49:09 -0300

    Desafio API concluido

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -1,6 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Models;
-
+using Microsoft.EntityFrameworkCore;
 namespace TrilhaApiDesafio.Context
 {
     public class OrganizadorContext : DbContext
